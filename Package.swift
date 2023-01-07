251b091ef0bc75629ab8891784495701b7723465
Wolf McNally <wolf@wolfmcnally.com>
2023-01-07 15:14:40 -0800

    Removed platform requirements.

--- Package.swift
+++ Package.swift
@@ -4,15 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "GraphMermaid",
-    platforms: [.macOS(.v13), .iOS(.v15)],
     products: [
         .library(
             name: "GraphMermaid",
             targets: ["GraphMermaid"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/WolfMcNally/WolfBase.git", .upToNextMajor(from: "4.0.0")),
-        .package(url: "https://github.com/WolfMcNally/Graph.git", branch: "master")
+        .package(url: "https://github.com/WolfMcNally/WolfBase.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/WolfMcNally/Graph.git", .upToNextMajor(from: "0.1.0"))
     ],
     targets: [
         .target(
